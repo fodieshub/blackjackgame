691e2a598ffbc23b3932769e4d5efec378f5e5b5
fodieshub <buzkenny@gmail.com>
2021-07-16 08:40:12 -0700

    Working on the Backend
    Created the renderGame() function that print out the state of the player and startGame() function that calls the renderGame() function to start the game.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Javascript is running")
+let firstCard = 11
+let secondCard = 8
+let message = ""
+let sum = firstCard + secondCard
+
+let messageEl = document.getElementById("message-el")
+
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+
+    if(sum < 21){
+        message = "Do you want to Draw new Card?"
+    }else if(sum === 21){
+        message = "Wahoo!, You have BLACKJACK"
+    }else{
+        message = "You are Out of the Game!!!"
+    }
+    messageEl.textContent = message
+}
