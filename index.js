62abf2f0929a78d9c2fe0b7a730dc79dc420b7a4
fodieshub <buzkenny@gmail.com>
2021-07-16 09:05:36 -0700

    Build
    Performed the sum of the two cards and displayed it
    
    The P tags for cards is working properly

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let message = ""
 let sum = firstCard + secondCard
 
 let messageEl = document.getElementById("message-el")
-
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
 
 function startGame(){
     renderGame()
@@ -13,11 +14,13 @@ function startGame(){
 function renderGame(){
 
     if(sum < 21){
-        message = "Do you want to Draw new Card?"
+        message = "Do you want to Draw a new Card?"
     }else if(sum === 21){
         message = "Wahoo!, You have BLACKJACK"
     }else{
         message = "You are Out of the Game!!!"
     }
+    cardEl.textContent = "Your Cards Are: " + firstCard +", " + secondCard
     messageEl.textContent = message
+    sumEl.textContent = " Sum: " + sum
 }
